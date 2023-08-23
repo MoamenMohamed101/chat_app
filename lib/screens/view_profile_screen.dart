71fcb4dcc2cf8d77b3c845ae078ba15d4cfe82d8
Moamen <mmoamenmohamed101@gmail.com>
2023-08-23 20:45:38 +0300

    chat_app

---
+++ lib/screens/view_profile_screen.dart
@@ -0,0 +1,262 @@
+import 'dart:developer';
+import 'dart:io';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:chat_app/Api/apis.dart';
+import 'package:chat_app/helper/dialogs.dart';
+import 'package:chat_app/main.dart';
+import 'package:chat_app/models/chat_user.dart';
+import 'package:chat_app/screens/auth/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProfileScreen extends StatefulWidget {
+  final ChatUser chatUser;
+
+  const ProfileScreen({Key? key, required this.chatUser}) : super(key: key);
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final formKey = GlobalKey<FormState>();
+  String? _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      // for hiding keyboard when user tap on screen
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('Profile Screen'),
+        ),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(bottom: 10),
+          child: FloatingActionButton.extended(
+            backgroundColor: Colors.red,
+            onPressed: () async {
+              Dialogs.showProgressBar(context);
+              Apis.updateActiveStatus(false);
+              await Apis.auth.signOut().then((value) async {
+                await GoogleSignIn().signOut().then((value) {
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                  Apis.auth = FirebaseAuth.instance;
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginScreen(),
+                    ),
+                  );
+                });
+              });
+            },
+            label: const Text('Logout'),
+            icon: const Icon(Icons.logout),
+          ),
+        ),
+        body: Form(
+          key: formKey,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: mq.height * .05),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: mq.width,
+                    height: mq.height * .03,
+                  ),
+                  Stack(
+                    children: [
+                      _image != null ? ClipRRect(
+                              borderRadius:
+                                  BorderRadius.circular(mq.height * .1),
+                              child: Image.file(
+                                File(_image!),
+                                height: mq.height * .2,
+                                width: mq.height * .2,
+                                fit: BoxFit.cover,
+                              ),
+                            ) : ClipRRect(
+                              borderRadius:
+                                  BorderRadius.circular(mq.height * .1),
+                              child: CachedNetworkImage(
+                                height: mq.height * .2,
+                                width: mq.height * .2,
+                                fit: BoxFit.cover,
+                                imageUrl: widget.chatUser.image!,
+                                errorWidget: (context, url, error) =>
+                                    const CircleAvatar(
+                                  child: Icon(CupertinoIcons.person),
+                                ),
+                              ),
+                            ),
+                      Positioned(
+                        right: 0,
+                        bottom: 0,
+                        child: MaterialButton(
+                          elevation: 1,
+                          shape: const CircleBorder(),
+                          color: Colors.white,
+                          onPressed: () {
+                            showButtonSheet();
+                          },
+                          child: const Icon(Icons.edit, color: Colors.blue),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: mq.height * .03,
+                  ),
+                  Text(
+                    widget.chatUser.email!,
+                    style: const TextStyle(color: Colors.black45, fontSize: 16),
+                  ),
+                  SizedBox(
+                    height: mq.height * .05,
+                  ),
+                  TextFormField(
+                    initialValue: widget.chatUser.name,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.person, color: Colors.blue),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      hintText: 'eg. Happy Singh',
+                      label: const Text('Name'),
+                    ),
+                    onSaved: (val) => Apis.me.name = val,// what is the use of onSaved? // onSaved is used to save the value of TextFormField in a variable
+                    validator: (val) =>
+                        val!.isNotEmpty ? null : 'Required Field',
+                  ),
+                  SizedBox(
+                    height: mq.height * .02,
+                  ),
+                  TextFormField(
+                    initialValue: widget.chatUser.about,
+                    decoration: InputDecoration(
+                      prefixIcon:
+                          const Icon(Icons.info_outline, color: Colors.blue),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      hintText: 'eg. Feeling Happy',
+                      label: const Text('About'),
+                    ),
+                    onSaved: (val) => Apis.me.about = val,
+                    validator: (val) =>
+                        val!.isNotEmpty ? null : 'Required Field',
+                  ),
+                  SizedBox(
+                    height: mq.height * .05,
+                  ),
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        shape: const StadiumBorder(),
+                        minimumSize: Size(mq.width * .5, mq.height * .06)),
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        formKey.currentState!.save();
+                        Apis.updateUserInfo().then((value) {
+                          Dialogs.showSnackBar(
+                            context,
+                            'Profile updated Successfully!',
+                          );
+                        });
+                      }
+                    },
+                    icon: const Icon(Icons.edit, size: 28),
+                    label: const Text(
+                      'UPDATE',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  showButtonSheet() {
+    showModalBottomSheet(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        context: context,
+        builder: (value) {
+          return ListView(
+            shrinkWrap: true,
+            padding:
+                EdgeInsets.only(top: mq.height * .03, bottom: mq.height * .07),
+            children: [
+              const Text(
+                'Pick Profile Picture',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: mq.height * .02,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: const CircleBorder(),
+                      fixedSize: Size(mq.width * .3, mq.height * .15),
+                    ),
+                    onPressed: () async {
+                      final ImagePicker picker = ImagePicker();
+                      final XFile? image = await picker.pickImage(
+                          source: ImageSource.gallery, imageQuality: 80);
+                      if (image != null) {
+                        log('Image Path ${image.path} -- MimeType: ${image.mimeType}');
+                        setState(() {
+                          _image = image.path;
+                        });
+                      }
+                      Apis.updateProfilePicture(File(_image!));
+                      Navigator.pop(context); // why warning ? because we are using context of showModalBottomSheet and we are popping the context of showModalBottomSheet
+                    },
+                    child: Image.asset('assets/images/add_image.png'),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: const CircleBorder(),
+                      fixedSize: Size(mq.width * .3, mq.height * .15),
+                    ),
+                    onPressed: () async {
+                      final ImagePicker picker = ImagePicker();
+                      final XFile? image = await picker.pickImage(
+                          source: ImageSource.camera, imageQuality: 80);
+                      if (image != null) {
+                        log('Image Path ${image.path}');
+                        setState(() {
+                          _image = image.path;
+                        });
+                      }
+                      Apis.updateProfilePicture(File(_image!));
+                      // ignore: use_build_context_synchronously
+                      Navigator.pop(context);
+                    },
+                    child: Image.asset('assets/images/camera.png'),
+                  ),
+                ],
+              ),
+            ],
+          );
+        });
+  }
+}
